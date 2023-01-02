465423e171ac79d6f15c2ad993d0ed2fc774788f
Steve <thantheinthwin.dev@gmail.com>
2023-01-02 22:15:33 +0630

    added active class to navbar with acitvePage.js

---
+++ public/navbar.php
@@ -0,0 +1,57 @@
+<html>
+    <head>
+        <title>Global Wild Swimming and Camping</title>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+        <script src="../public/scripts/scrollFadeIn.js" defer></script>
+        <script src="../public/scripts/activePage.js" defer></script>
+
+        <link rel="icon" type="image/x-icon" href="../src/logo and icons/logo.svg">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="../public/global.css">
+    </head>
+    <body>
+        <!-- Nav bar -->
+        <nav class="navbar navbar-expand-lg bg-light sticky-top w-100 shadow">
+            <div class="container-fluid">
+                <a class="navbar-brand">
+                    <img src="../src/logo and icons/logo.svg" alt="GWSC" style="width: 3rem;">
+                    <span class="h3 d-xl-none align-middle">GWSC</span>
+                    <span class="h3 d-none d-xl-inline mx-2 align-middle">Global Wild Swimming and Camping</span>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end fs-5" id="navbarScroll">
+                    <ul class="navbar-nav my-2 my-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="../public/index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="../public/information.php">About Us</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Services</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Separated link</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Sign in</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex my-2 mx-lg-4" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </body>
+</html>
---
+++ public/scripts/activePage.js
@@ -0,0 +1,6 @@
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('.nav-link').forEach(link => {
+    if(link.href.includes(`${activePage}`)){
+        link.classList.add('active');
+    }
+})
